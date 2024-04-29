8e8e664147235de4dcc97e326f7d00b7601254a1
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-04-29 13:55:45 -0700

    Config and more sprites

--- Tetrapak.lua
+++ Tetrapak.lua
@@ -31,6 +31,9 @@ Tetrapak = {}
 
 
 function SMODS.INIT.TetrapakJokers()
+
+
+    Load_Config()
     
     local mod = SMODS.findModByID(TETRAPAKID)
 
@@ -63,8 +66,11 @@ function SMODS.INIT.TetrapakJokers()
     local jokerFiles = love.filesystem.getDirectoryItems(mod.path.."jokers")
     local jokerdefs = {}
     for k, file in pairs(jokerFiles) do
-        if string.find(file, ".lua") then
+        lowercasename = string.sub(file, 1, string.len(file) - 4):lower()
+        print(lowercasename)
+        if string.find(file, ".lua") and G.TETRAPAK_Config.Enabled[lowercasename] then
             local joker = love.filesystem.load(mod.path.."jokers/"..file)()
+
             table.insert(jokerdefs, joker)
 
         end
@@ -74,7 +80,7 @@ function SMODS.INIT.TetrapakJokers()
     local spectralFiles = love.filesystem.getDirectoryItems(mod.path.."spectrals")
     local spectraldefs = {}
     for k, file in pairs(spectralFiles) do
-        if string.find(file, ".lua") then
+        if string.find(file, ".lua") and G.TETRAPAK_Config.Enabled[string.sub(file, 1, string.len(file) - 4):lower()] then
             local spectral = love.filesystem.load(mod.path.."spectrals/"..file)()
             table.insert(spectraldefs, spectral)
         end
@@ -101,20 +107,30 @@ function SMODS.INIT.TetrapakJokers()
 
     
     for k, joker in pairs(Tetrapak.Jokers) do
+
         joker:register()
+
     end
 
     for k, spectral in pairs(Tetrapak.Spectrals) do
-        spectral:register()
+       
+            spectral:register()
+        
+
     end
 
     for k, jokerdef in pairs(jokerdefs) do
+        
         jokerdef:load_effect()
+        
     end
 
     for k, spectraldef in pairs(spectraldefs) do
+        
         spectraldef:load_effect()
+        
     end
+
     
     Load_atlas()
 
@@ -197,3 +213,76 @@ end
 
 
 
+
+function Load_Config()
+    local mod = SMODS.findModByID(TETRAPAKID)
+    
+ 
+    if not love.filesystem.getInfo(mod.path.."config.lua") then
+        print("No config file found for Tetrapak, creating")
+        local file = love.filesystem.newFile(mod.path.."config.lua")
+        local allcardnames = {}
+        for k, v in pairs(love.filesystem.getDirectoryItems(mod.path.."jokers")) do
+            table.insert(allcardnames, string.sub(v, 1, string.len(v) - 4):lower())
+        end
+
+        for k, v in pairs(love.filesystem.getDirectoryItems(mod.path.."spectrals")) do
+            table.insert(allcardnames, string.sub(v, 1, string.len(v) - 4):lower())
+        end
+
+        local conf = GetDefaultConfig()
+        for k, v in pairs(allcardnames) do
+            conf.Enabled[v] = true
+        end
+
+        file:open("w")
+        file:write("return " .. table.tostring(conf))
+    end
+
+    local config = love.filesystem.load(mod.path.."config.lua")()
+
+    G.TETRAPAK_Config = config
+end
+
+
+function Save_Config()
+    local mod = SMODS.findModByID(TETRAPAKID)
+    local file = love.filesystem.newFile(mod.path.."config.lua")
+    file:open("w")
+    file:write("return " .. table.tostring(G.TETRAPAK_Config))
+end
+
+function GetDefaultConfig()
+   
+        return {
+            info = [[
+                This is the config file for Tetrapak. 
+                Set the value of each card to true to enable it, or false to disable it.
+                If you want to disable a card, you can also just delete the file from the jokers or spectrals folder.
+
+                there will be more options in the future.
+            ]],
+            Enabled = {
+
+            }
+        }
+    
+end
+
+
+function table.tostring(tbl, depth)
+    if not depth then
+        depth = 1
+    end
+    local str = "{\n"
+    local indents = str.rep("    ", depth)
+    for k, v in pairs(tbl) do
+        if type(v) == "table" then
+            str = str .. indents .. k .. "=" .. table.tostring(v, depth + 1) .. ",\n"
+        else
+            str = str ..indents.. k .. "=" .. tostring(v) .. ",\n"
+        end
+    end
+    str = str .. "}"
+    return str
+end
